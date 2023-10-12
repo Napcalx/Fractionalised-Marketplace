f9acd1f15cf5be5ea0588363d1703859e360fb28
Napcalx <karlike24@gmail.com>
2023-10-12 20:01:40 +0100

    A few chanegs

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -3,15 +3,13 @@ pragma solidity ^0.8.13;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "./Andret.sol";
 
 contract Marketplace {
-    IERC721 public Andret; // The ERC721 NFT contract
-
+    Andret _Andret; // The ERC721 NFT contract
     uint256 public feePercent = 10; // 0.1% fee
-
     struct Listing {
         address seller;
-        address token;
         uint256 tokenId;
         uint256 totalFractions;
         uint256 fractionsAvailable;
@@ -28,27 +26,25 @@ contract Marketplace {
     event FractionPurchased(uint256 Id, address buyer, uint256 fractionsPurchased);
     event FractionTransferred(uint256 Id, address from, address to, uint256 fractionsTransferred);
 
-    constructor(address _andret) {
-        Andret = IERC721(_andret);
+    constructor() {
         admin = msg.sender;
     }
 
-    function list(uint256 _tokenId, uint256 _totalFractions) external {
+    function list(uint256 _Id, uint256 _totalFractions) external {
         require(_totalFractions > 0, "Total fractions must be greater than 0");
-        require(Andret.ownerOf(_tokenId) == msg.sender, "Only NFT owner can list");
-        Andret.transferFrom(msg.sender, address(this), _tokenId);
+        require(_Andret.ownerOf(_Id) == msg.sender, "Only NFT owner can list");
+        _Andret.transferFrom(msg.sender, address(this), _Id);
 
         Id = listings.length;
         listings.push(Listing({
             seller: msg.sender,
-            token: _andret,
-            tokenId: _tokenId,
+            tokenId: _Id,
             totalFractions: _totalFractions,
             fractionsAvailable: _totalFractions,
             active: false
 
         }));
-        emit Listed(Id, msg.sender, _tokenId, _totalFractions);
+        emit Listed(Id, msg.sender, _Id, _totalFractions);
     }
 
     function purchaseFractions(uint256 _Id, uint256 _fractionsToPurchase) payable external {
@@ -57,7 +53,7 @@ contract Marketplace {
         uint256 cost = (_fractionsToPurchase * listing.totalFractions) / listing.fractionsAvailable;
         uint256 fee = (cost * feePercent) / 10000; // 0.1% fee
 
-        payable(msg.sender).transferFrom(msg.sender, address(this), cost + fee);
+        payable(seller).transferFrom(msg.sender, address(this), cost + fee);
         payable(msg.sender).transfer(msg.value, fee);
         Andret.transferFrom(address(this), msg.sender, listing.tokenId);
         Fractions[_Id][msg.sender] += _fractionsToPurchase;
@@ -65,7 +61,7 @@ contract Marketplace {
         emit FractionPurchased(_Id, msg.sender, _fractionsToPurchase);
     }
 
-    function transferFractions(uint256 _Id, address _to, uint256 _fractionsToTransfer) payable external {
+    function transferFractions(uint256 _Id, address _to, uint256 _fractionsToTransfer) external {
         require(Fractions[_Id][msg.sender] >= _fractionsToTransfer, "Not enough fractions to transfer");
         Fractions[_Id][msg.sender] -= _fractionsToTransfer;
         Fractions[_Id][_to] += _fractionsToTransfer;
